50c6a650884fddd3c12bc138918350aa7b39e25a
DanielMcCrystal <d.mccrystal512@gmail.com>
2015-04-04 16:10:12 -0400

    Replaced single player view controller

---
+++ TheWordGame/TheWordGame/SinglePlayerFreePlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  SinglePlayerFreePlayViewController.h
+//  TheWordGame
+//
+//  Created by Daniel McCrystal on 4/4/15.
+//  Copyright (c) 2015 Daniel McCrystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SinglePlayerFreePlayViewController : UIViewController
+
+@end
---
+++ TheWordGame/TheWordGame/SinglePlayerFreePlayViewController.m
@@ -0,0 +1,38 @@
+//
+//  SinglePlayerFreePlayViewController.m
+//  TheWordGame
+//
+//  Created by Daniel McCrystal on 4/4/15.
+//  Copyright (c) 2015 Daniel McCrystal. All rights reserved.
+//
+
+#import "SinglePlayerFreePlayViewController.h"
+
+@interface SinglePlayerFreePlayViewController ()
+
+@end
+
+@implementation SinglePlayerFreePlayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"got here");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end
