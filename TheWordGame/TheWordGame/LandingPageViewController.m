1fe62ad23cc91f7fb7854bc2c0c8006eb10cbf2e
cowboylynk <cowboylynk@me.com>
2015-04-06 16:22:46 -0400

    Revert 55cafb6..820cf19
    This rolls back to commit 55cafb631befc1b46e9743cd4db9de6227076323.

--- TheWordGame/TheWordGame/LandingPageViewController.m
+++ TheWordGame/TheWordGame/LandingPageViewController.m
@@ -50,14 +50,14 @@
     
     multiplayer.animationImages =
     [NSArray arrayWithObjects:
-     [UIImage imageNamed:@"mp1.png"],
-     [UIImage imageNamed:@"mp2.png"],
-     [UIImage imageNamed:@"mp3.png"],
-     [UIImage imageNamed:@"mp4.png"],
-     [UIImage imageNamed:@"mp5.png"],
-     [UIImage imageNamed:@"mp6.png"],
-     [UIImage imageNamed:@"mp7.png"],
-     [UIImage imageNamed:@"mp8.png"], nil];
+     [UIImage imageNamed:@"sp1.png"],
+     [UIImage imageNamed:@"sp2.png"],
+     [UIImage imageNamed:@"sp3.png"],
+     [UIImage imageNamed:@"sp4.png"],
+     [UIImage imageNamed:@"sp5.png"],
+     [UIImage imageNamed:@"sp6.png"],
+     [UIImage imageNamed:@"sp7.png"],
+     [UIImage imageNamed:@"sp8.png"], nil];
     multiplayer.animationDuration = 0.7;
     [multiplayer setAnimationRepeatCount:1];
     [multiplayer startAnimating];
