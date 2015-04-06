df2e18d637aa8c5c340dd413ccb92feb25ddc1d7
Rio Lynk <kinglynk@gmail.com>
2015-04-06 15:45:51 -0400

    Added new view controller

---
+++ TheWordGame/TheWordGame/MultiplayerPassandPlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  MultiplayerPassandPlayViewController.h
+//  TheWordGame
+//
+//  Created by Rio Lynk on 4/6/15.
+//  Copyright (c) 2015 Daniel McCrystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultiplayerPassandPlayViewController : UIViewController
+
+@end
