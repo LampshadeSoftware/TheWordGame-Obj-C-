1a25ce0d83a12cb92c96bde34d467f866ff7fd00
Rio Lynk <kinglynk@gmail.com>
2015-04-09 16:25:32 -0400

    added multiplayer pass and play game page

--- TheWordGame/TheWordGame/MultiplayerPassAndPlayPlayerInputViewController.m
+++ TheWordGame/TheWordGame/MultiplayerPassAndPlayPlayerInputViewController.m
@@ -16,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _PPplayerNames = [NSMutableArray arrayWithObjects:@"", @"", @"", @"", @"", @"", @"", @"", nil];
     _playerNameInput.returnKeyType = UIReturnKeyDone;
     [_playerNameInput becomeFirstResponder];
     _playerNameInput.delegate = self;
@@ -26,16 +25,17 @@
 
 - (BOOL) textFieldShouldReturn:(UITextField *)textField {
     
+    
     [_PPplayerNames addObject:_playerNameInput.text];
     
-    _player1Label.text = _PPplayerNames[[_PPplayerNames count] -1];
-    _player2Label.text = _PPplayerNames[[_PPplayerNames count] -2];
-    _player3Label.text = _PPplayerNames[[_PPplayerNames count] -3];
-    _player4Label.text = _PPplayerNames[[_PPplayerNames count] -4];
-    _player5Label.text = _PPplayerNames[[_PPplayerNames count] -5];
-    _player6Label.text = _PPplayerNames[[_PPplayerNames count] -6];
-    _player7Label.text = _PPplayerNames[[_PPplayerNames count] -7];
-    _player8Label.text = _PPplayerNames[[_PPplayerNames count] -8];
+    _player1Label.text = _PPplayerNames[1];
+    _player2Label.text = _PPplayerNames[2];
+    _player3Label.text = _PPplayerNames[3];
+    _player4Label.text = _PPplayerNames[4];
+    _player5Label.text = _PPplayerNames[5];
+    _player6Label.text = _PPplayerNames[6];
+    _player7Label.text = _PPplayerNames[7];
+    _player8Label.text = _PPplayerNames[8];
     
     return YES;
 }
