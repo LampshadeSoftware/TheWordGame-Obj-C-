8ba6e5cbf6febeb18443697f70d2580cd2d41802
Daniel McCrystal <d.mccrystal512@gmail.com>
2015-04-10 14:59:55 -0400

    Documented SinglePlayerFreePlayView

--- TheWordGame/TheWordGame/SinglePlayerFreePlayViewController.m
+++ TheWordGame/TheWordGame/SinglePlayerFreePlayViewController.m
@@ -21,23 +21,35 @@
     // Do any additional setup after loading the view.
     NSMutableArray *savedGames = [LSSGame getSavedSinglePlayerFreePlayGames];
     
+    // If there are no saved games...
     if([savedGames count] == 0) {
+        // Set the current game to a new game
         _currentGame = [[LSSGame alloc] init];
+        // Add the current game to the array of saved games
         [savedGames addObject:_currentGame];
     }
+    // If there are saved games but currentGame is not set to any of them
     else if(_currentGame == nil){
+        // Set the current game to the last saved game
         _currentGame = savedGames[[savedGames count]-1];
     }
     
+    // Display the current word
     _currentWord.text = _currentGame.currentWord;
+    // Set the trail words to the last 3 played words (blank strings in a new game)
     _lastWord0.text = _currentGame.usedWords[[_currentGame.usedWords count] - 2];
     _lastWord1.text = _currentGame.usedWords[[_currentGame.usedWords count] - 3];
     _lastWord2.text = _currentGame.usedWords[[_currentGame.usedWords count] - 4];
+    // Display the current log message
     _invalidWordLog.text = _currentGame.logMessage;
+    // Display the current word count
     _wordCountingLabel.text = [NSString stringWithFormat:@"%d", _currentGame.wordCount];
     
+    // Set the return key to a done key
     _inputTextBox.returnKeyType = UIReturnKeyDone;
+    // Bring the keyboard up automatically
     [_inputTextBox becomeFirstResponder];
+    // No idea what this does, but we need it there
     _inputTextBox.delegate = self;
 }
 
@@ -47,28 +59,41 @@
 }
 
 - (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    // If the user played a valid word on the current word...
     if([_currentGame isValidPlay:_inputTextBox.text onWord:_currentGame.currentWord]) {
+        // Clear the game's log message
         _currentGame.logMessage = @"";
+        // Clear the display log
         _invalidWordLog.text = @"";
+        // Set the displayed current word to the word entered by the user
         _currentWord.text = _inputTextBox.text;
+        // Set the game's current word to the word entered by the user
         _currentGame.currentWord = _inputTextBox.text;
+        // Update the trail words to show the last 3 words played
         _lastWord0.text = _currentGame.usedWords[[_currentGame.usedWords count] - 1];
         _lastWord1.text = _currentGame.usedWords[[_currentGame.usedWords count] - 2];
         _lastWord2.text = _currentGame.usedWords[[_currentGame.usedWords count] - 3];
+        // Add the word entered by the user to the game's array of used words
         [_currentGame.usedWords addObject:_inputTextBox.text];
+        // Increment the game's word count
         _currentGame.wordCount++;
-        NSString *wordCounter = [NSString stringWithFormat:@"%d", _currentGame.wordCount];
-        _wordCountingLabel.text = wordCounter;
+        // Display the game's word count
+        _wordCountingLabel.text = [NSString stringWithFormat:@"%d", _currentGame.wordCount];
     }
+    // User did NOT play a valid word on the current word
     else {
+        // Display the game's log message
         _invalidWordLog.text = _currentGame.logMessage;
     }
+    // Clear the input text box
     _inputTextBox.text = @"";
     
     return YES;
 }
 - (IBAction)generateNewWord:(id)sender {
+    // Set the current game to a new default game, which starts with a SmartRandom generated word
     _currentGame = [[LSSGame alloc] init];
+    // Re-perform initial setup
     [self viewDidLoad];
 }
 
